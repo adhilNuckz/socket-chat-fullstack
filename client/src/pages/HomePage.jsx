7ca23e672b143e711c86308ac155c0ccb8c18d8b
aadhilNuckz <mikeadhil2002@gmail.com>
2025-08-17 10:44:45 -0700

    added side bar and contaiiners

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import RightSidebar from '../components/RightSidebar'
+import ChatContainer from '../components/ChatContainer'
+
+const HomePage = () => {
+  return (
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+
+      <div className='backdrop-blur-xl border-2 border-white-600 
+      rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
+        <Sidebar />
+        <ChatContainer /> 
+        <RightSidebar />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
